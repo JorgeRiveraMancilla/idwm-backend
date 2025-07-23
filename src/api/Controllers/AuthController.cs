1cf79c75a5c4640c034bb4894f82ea45d8b60adc
NachoXx25 <ignacio.alfonso2002@gmail.com>
2025-07-22 22:40:03 -0400

    feat(controllers): make baseController and authController with login functionality

---
+++ src/api/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Logging;
+using Tienda_UCN_api.src.Application.DTO;
+using Tienda_UCN_api.src.Application.Services.Interfaces;
+
+namespace Tienda_UCN_api.src.api.Controllers
+{
+    /// <summary>
+    /// Controlador de autenticación.
+    /// </summary>
+    public class AuthController : BaseController
+    {
+        /// <summary>
+        /// Servicio de usuarios.
+        /// </summary>
+        private readonly IUserService _userService;
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// Inicia sesión con el usuario proporcionado.
+        /// </summary>
+        /// <param name="loginDTO">DTO que contiene las credenciales del usuario.</param
+        /// <returns>Un IActionResult que representa el resultado de la operación.</returns>
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+        {
+            var token = await _userService.Login(loginDTO);
+            return Success(new { Token = token }, "Inicio de sesión exitoso");
+        }
+    }
+}
